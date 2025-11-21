8ee86ea77239913751bb9b08923147fd472cb2c8
thomasball42 <tsb42@cam.ac.uk>
2025-11-21 10:38:26 +0000

    Function for running main

--- main.py
+++ main.py
@@ -24,12 +24,9 @@ from provenance._get_impacts_bd import get_impacts as get_impacts_main
 from provenance._process_dat import main as process_dat_main
 # from provenance.global_commodity_impacts import main as global_commodity_impacts_main
 
-
-
 # CONFIG
 # Select years for which to calculate the results 
 YEARS = list(range(1986, 2022))
-# YEARS = [2019]
 
 # Select a conversion method
 # inputs: ("dry_matter", "Energy", "Protein", "Fiber_TD", "Zinc", "Iron", "Calcium",
@@ -42,26 +39,29 @@ CONVERSION_OPTION = "dry_matter"
 PREFER_IMPORT = "import"
 
 # select working directory
-WORKING_DIR = '/home/louis/Documents/zoology/pipeline/mrio'
+WORKING_DIR = '.'
 
 # 0 = all, 1 = unzip, 2 = trade matrix, 3 = animal products to feed, 4 = area calculation, 5 = country impacts
 PIPELINE_COMPONENTS:list = [0]
 # PIPELINE_COMPONENTS:list = [5]
 
-
-
 from pandas import read_excel, read_csv
 cdat = read_excel("input_data/nocsDataExport_20251021-164754.xlsx")
 COUNTRIES = [_.upper() for _ in cdat["ISO3"].unique().tolist() if isinstance(_, str)]
-COUNTRIES = ["USA", "IND", "BRA", "JPN", "UGA", "GBR"]
-# COUNTRIES = ["GBR"]
+# COUNTRIES = ["USA", "IND", "BRA", "JPN", "UGA", "GBR"]
 
 ##########################################################
 
-
-def main():
+def main(years=list(range(1986, 2022)),
+         conversion_option="dry_matter",
+         prefer_import="import",
+         pipeline_components=[0],
+         working_dir=".",
+         countries = ["GBR"],
+         results_dir="./results"):
+    
     os.system('cls' if os.name == 'nt' else 'clear')
-    os.chdir(WORKING_DIR)
+    os.chdir(working_dir)
 
     component_dict = {
         0: "Full pipeline",
@@ -72,12 +72,12 @@ def main():
         5: "Country-level provenance calculations",}
 
     print(f"""\nStarting MRIO calculations with options:
-    Working directory: {WORKING_DIR}
-    Using {CONVERSION_OPTION} as the conversion option
-    Preferring {PREFER_IMPORT} data
-    Running pipeline component {[p for p in PIPELINE_COMPONENTS]}: {[component_dict[p] for p in PIPELINE_COMPONENTS]} 
+    Working directory: {working_dir}
+    Using {conversion_option} as the conversion option
+    Preferring {prefer_import} data
+    Running pipeline component {[p for p in pipeline_components]}: {[component_dict[p] for p in pipeline_components]} 
 
-    Years to process: {YEARS}
+    Years to process: {years}
     """)
 
     # Create directories
@@ -85,7 +85,7 @@ def main():
     results_dir.mkdir(exist_ok=True)
 
 
-    if (0 in PIPELINE_COMPONENTS) or (1 in PIPELINE_COMPONENTS):
+    if (0 in pipeline_components) or (1 in pipeline_components):
         print("Unzipping data...")
         try:
             unzip_data("./input_data") # Uncomment this line to enable unzipping
@@ -94,10 +94,10 @@ def main():
             print(f"Error during data unzipping: {e}")
             # Continue anyway as data might already be unzipped
 
-    if PIPELINE_COMPONENTS == [1]:
+    if pipeline_components == [1]:
         return
     
-    for year in YEARS:
+    for year in years:
 
         year_dir = Path(f"./results/{year}")
         year_dir.mkdir(exist_ok=True)
@@ -108,22 +108,22 @@ def main():
         
         hist = "Historic" if year < 2010 else ""
 
-        if (0 in PIPELINE_COMPONENTS) or (2 in PIPELINE_COMPONENTS):
+        if (0 in pipeline_components) or (2 in pipeline_components):
             calculate_trade_matrix(
-                conversion_opt=CONVERSION_OPTION,
-                prefer_import=PREFER_IMPORT,
+                conversion_opt=conversion_option,
+                prefer_import=prefer_import,
                 year=year,
                 historic=hist)
             
-        if (0 in PIPELINE_COMPONENTS) or (3 in PIPELINE_COMPONENTS):
+        if (0 in pipeline_components) or (3 in pipeline_components):
             animal_products_to_feed(
-                prefer_import=PREFER_IMPORT,
-                conversion_opt=CONVERSION_OPTION,
+                prefer_import=prefer_import,
+                conversion_opt=conversion_option,
                 year=year,
                 historic=hist)
             
-        if (0 in PIPELINE_COMPONENTS) or (4 in PIPELINE_COMPONENTS):
-            if 4 in PIPELINE_COMPONENTS:
+        if (0 in pipeline_components) or (4 in pipeline_components):
+            if 4 in pipeline_components:
                 print("    MRIO area calculation is deprecated")
             else:
                 print("   MRIO complete") 
@@ -134,7 +134,7 @@ def main():
             #     year=year)
             
 
-        if (0 in PIPELINE_COMPONENTS) or (5 in PIPELINE_COMPONENTS):
+        if (0 in pipeline_components) or (5 in pipeline_components):
             print("    Processing country-level provenance and impacts...")
             missing_items = []
             if hist == "Historic":
@@ -142,7 +142,7 @@ def main():
             else:
                 sua = read_csv(f"./input_data/SUA_Crops_Livestock_E_All_Data_(Normalized).csv", encoding="latin-1", low_memory=False)
 
-            for country in COUNTRIES:
+            for country in countries:
                 print(f"    Processing country: {country}")
                 t0 = time.perf_counter()
                 cons, feed = consumption_provenance_main(year, country, sua, hist)
@@ -168,7 +168,12 @@ def main():
         print(f"Year {year} processing completed successfully\n")
 
 
-
-
 if __name__ == "__main__":
-    main()
+    main(   
+        years=YEARS,
+        conversion_option=CONVERSION_OPTION,
+        prefer_import=PREFER_IMPORT,
+        pipeline_components=PIPELINE_COMPONENTS,
+        working_dir=WORKING_DIR,
+        countries=COUNTRIES
+    )
